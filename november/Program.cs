e0f32fa25c1602c5f4db61f8c83a55b6a4dda163
christos <potamitischristos@gmail.com>
2020-11-26 15:21:58 +0100

    1

--- november/Program.cs
+++ november/Program.cs
@@ -1,80 +1,81 @@
 ﻿using System;
 using Raylib_cs;
 
+Raylib.InitWindow(1920, 1080, "Bil spel");
+Texture2D background = Raylib.LoadTexture("highway.png");
+Texture2D car = Raylib.LoadTexture("car.png");
 
-namespace november
+
+
+string scene = "intro";
+float x = 0;
+float y = 0;
+
+
+
+
+
+
+while (!Raylib.WindowShouldClose())
 {
-    class Program
+
+   
+
+    if (scene == "intro")
     {
-        static void Main(string[] args)
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
         {
-            Texture2D highway = Raylib.LoadTexture("highway.png");
-
-            Raylib.InitWindow(800, 600, "Bil spel");
-
-
-            string scene = "intro";
-
-
-            while (!Raylib.WindowShouldClose())
-            {
-
-
-                if (scene == "intro")
-                {
-                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
-                    {
-                        scene = "game";
-                    }
-                }
-                else if (scene == "game")
-                {
-                    if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
-                    {
-
-                    }
-                    else if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
-                    {
-
-                    }
-                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_TAB))
-                    {
-                        scene = "intro";
-                    }
-                }
-
-                // DRAWING:
-                Raylib.BeginDrawing();
-
-                if (scene == "intro")
-                {
-                    Raylib.ClearBackground(Color.LIME);
-                }
-                else if (scene == "game")
-                {
-                    Raylib.ClearBackground(Color.MAROON);
-                    Raylib.DrawTexture(highway, 800, 600, Color.WHITE);
-                }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                Raylib.EndDrawing();
-            }
-
+            scene = "game";
         }
     }
+    else if (scene == "game")
+    {
+      
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            x += 1.5f;
+        }
+
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+        {
+            x -= 1.5f;
+        }
+
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+        {
+            y += 1.5f;
+
+        }
+
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+        {
+            y -= 1.5f;
+
+        }
+       
+
+        
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_TAB))
+        {
+            scene = "intro";
+        }
+    }
+
+    // DRAWING:
+    Raylib.BeginDrawing();
+
+    if (scene == "intro")
+    {
+        Raylib.ClearBackground(Color.LIME);
+    }
+    else if (scene == "game")
+    {
+        Raylib.ClearBackground(Color.MAROON);
+        Raylib.DrawTexture(background, 0, 0, Color.WHITE);
+        Raylib.DrawTexture(car, (int)x, (int)y, Color.WHITE);
+    
+        
+    }
+    Raylib.EndDrawing();
 }
